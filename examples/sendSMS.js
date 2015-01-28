e8b93da17512f62e2b239f784b9801549515d189
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-01-28 17:52:05 +0700

    change sendSMS

--- examples/sendSMS.js
+++ examples/sendSMS.js
@@ -1,17 +1,19 @@
 var osms = require('../index');
 var sms = new osms({host: 'localhost'});
 
-var op = {text: 'Привет привет привет '};
+var op1 = {text: 'Привет привет привет привет привет привет привет привет привет привет привет привет привет привет привет привет привет привет привет'};
+var op2 = {text: 'Hello, my darling!! How do you do? Im fine. Hello, my darling!! How do you do? Im fine. Hello, my darling!! How do you do? Im fine. Hello, my darling!! How do you do? Im fine. Hello, my darling!! How do you do? Im fine.'};
 
-console.log(op['text'].length);
+
+console.log(op2['text'].length);
+console.log('ascii', sms.isASCII(op2.text));
 
 
 sms.open(function () {
-    sms.sendSMS({span: 1, number: '89135292926', text: op['text']}, function (response) {
+    sms.sendSMS({span: 1, number: '89135292926', text: op2['text']}, function (response) {
         console.log(response, 'Done!');
     });
 });
-
 
 
 /*
--- index.js
+++ index.js
@@ -2,120 +2,151 @@ var Nami = require("nami");
 
 var osms = function (options) {
 
-	var options = options || {} ;
-	this.MAX_ONE_SMS_SIZE = options['MAX_ONE_SMS_SIZE'] || 67;
+    var options = options || {} ;
+    this.SMS_SIZE = 160;
+    this.CSMS_SIZE = 160 - 8;
 
-	this.ami = new Nami.Nami({
-		host: options['host'] || 'localhost',
-		port: options['port'] || 5038,
-		username: options['username'] || 'admin',
-		secret: options['secret'] || 'admin'
-	});
+    this.ami = new Nami.Nami({
+        host: options['host'] || 'localhost',
+        port: options['port'] || 5038,
+        username: options['username'] || 'admin',
+        secret: options['secret'] || 'admin'
+    });
 };
 
 osms.prototype.on = function (evt, callback) {
-	this.ami.on(evt, callback);
+    this.ami.on(evt, callback);
 };
 
 osms.prototype.open = function (callback) {
-	this.on('namiConnected', callback);
-	this.ami.open();
+    this.on('namiConnected', callback);
+    this.ami.open();
 };
 
 osms.prototype.send = function (action, callback) {
-	this.ami.send(action, callback);
+    this.ami.send(action, callback);
 };
 
 osms.prototype.validateOptionsForOneSMS = function (options) {
-	if (!options || !options['span'] || !options['number'] || !options['text']){
-		throw new Error('No required options');
-	}
-	if (options['text'].length > this.MAX_ONE_SMS_SIZE){
-		throw new Error('SMS size is over');
-	}
-	return true;
+    if (!options || !options['span'] || !options['number'] || 
+        !options['text'] || !options['timeout']) {
+
+        throw new Error('No required options');
+    }
+
+    if (options['text'].length > this.SMS_SIZE) {
+        throw new Error('SMS size is over');
+    }
+    
+    return true;
 };
 
-osms.prototype.getSendSMSCommand = function (options) {	
-	return [
-		'gsm send sync sms',
-		options['span'],
-		options['number'],
-		'"' + options['text'] + '"',
-		options['timeout']
-		].join(' ');
+osms.prototype.validateOptionsForCSMS = function (options) {
+    if (!options || !options['span'] || !options['number'] || 
+        !options['text'] || !options['smscount'] ||
+        !options['smssequence'] || !options['timeout']) {
+        console.log('options', options);
+        throw new Error('No required options');
+    }
+
+    if (options['text'].length > this.CSMS_SIZE) {
+        throw new Error('CSMS size is over');
+    }
+    
+    return true;
+};
+
+osms.prototype.getSendSMSCommand = function (options) { 
+    return [
+        'gsm send sync sms',
+        options['span'],
+        options['number'],
+        '"' + options['text'] + '"',
+        options['timeout']
+        ].join(' ');
 };
 
 osms.prototype.getSendCSMSCommand = function (options) {
-	return [
-		'gsm send sync csms',
-		options['span'],
-		options['number'],
-		'"' + options['text'] + '"',
-		options['flag'],
-		options['smscount'],
-		options['smssequence'],
-		options['timeout']
-		].join(' ');
+    return [
+        'gsm send sync csms',
+        options['span'],
+        options['number'],
+        '"' + options['text'] + '"',
+        options['flag'],
+        options['smscount'],
+        options['smssequence'],
+        options['timeout']
+        ].join(' ');
 };
 
 osms.prototype.sendOneSMS = function (options, callback) {
-	this.validateOptionsForOneSMS(options);
-	options['timeout'] = options['timeout'] || '20';
+    options['timeout'] = options['timeout'] || '20';
+    this.validateOptionsForOneSMS(options);
 
-	var Command = new Nami.Actions.Command();
-	Command.command = this.getSendSMSCommand(options);
-	this.send(Command, callback);
+    var Command = new Nami.Actions.Command();
+    Command.command = this.getSendSMSCommand(options);
+    this.send(Command, callback);
 };
 
 osms.prototype.sendCSMS = function (options, callback) {
-	//this.validateOptionsForOneSMS(options);
-	options['timeout'] = options['timeout'] || '20';
+    this.validateOptionsForCSMS(options);
 
-	var Command = new Nami.Actions.Command();
-	Command.command = this.getSendCSMSCommand(options);
-	this.send(Command, callback);
+    var Command = new Nami.Actions.Command();
+    Command.command = this.getSendCSMSCommand(options);
+    this.send(Command, callback);
 };
 
+osms.prototype.isASCII = function (str) {
+    return /^[\x00-\x7F]*$/.test(str);
+};
 
 osms.prototype.splitText = function (text) {
-	var array = text.match(/.{1,67}/g);
-	return array;
+    return text.match(new RegExp('.{1,' + this.CSMS_SIZE + '}', 'g'));
 };
 
 osms.prototype.sendLongSMS = function (options, callback) {
-	var responses = [];
-	var array = this.splitText(options['text']);
+    var responses = [],
+        array = this.splitText(options['text']);
 
-	array.map(function (text, i, array) {
-		opts = {
-			span: options['span'],
-			number: options['number'],
-			text: text,
-			flag: 0,
-			smscount: array.length,
-			smssequence: i + 1,
-			timeout: options['timeout'] || 20
-		};
+    array.map(function (text, i, array) {
+        opts = {
+            span: options['span'],
+            number: options['number'],
+            text: text,
+            flag: 0,
+            smscount: array.length,
+            smssequence: i + 1,
+            timeout: options['timeout'] || 20
+        };
 
-		this.sendCSMS(opts, function (response) {
-			responses.push(response);
-			if (i == array.length - 1) {
-				callback(responses);
-			}
-		});
+        this.sendCSMS(opts, function (response) {
+            responses.push(response);
+            if (i == array.length - 1) {
+                callback(responses);
+            }
+        });
 
-	}, this);
+    }, this);
+};
+
+osms.prototype.setSMSsize = function (text){
+    if (!this.isASCII(text)) {  //default 7bit        
+        this.SMS_SIZE = 70;     //16bit
+        this.CSMS_SIZE = 70 - 3;
+    }
 };
 
 osms.prototype.sendSMS = function (options, callback) {
-	if (options && options['text']) {
-		if (options['text'].length > this.MAX_ONE_SMS_SIZE) {
-			this.sendLongSMS(options, callback);
-		} else {
-			this.sendOneSMS(options, callback);
-		}
-	}
+    if (options && options['text']) {
+        
+        this.setSMSsize(options['text']);
+
+        if (options['text'].length > this.SMS_SIZE) {
+            this.sendLongSMS(options, callback);
+        } else {
+            this.sendOneSMS(options, callback);
+        }
+    }
 };
 
 module.exports = osms;
